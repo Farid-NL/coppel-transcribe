cb6227eb560961a5a1b56b61f170f2144015a94d
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2024-09-09 11:06:50 -0600

    feat[windows]: use zip file as the base of all operations

--- transcribe/main.py
+++ transcribe/main.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 
 import typer
 from typing import Optional
@@ -10,31 +11,33 @@ app = typer.Typer()
 err_console = Console(stderr=True)
 
 
-# TODO: Append programas file processed to outfile
 # TODO: Add 'Etapa' argument to command
 @app.command()
-def windows(
-    ip_directory: Annotated[str, typer.Argument()],
-    convert: Annotated[bool, typer.Option()] = True,
-):
-    full_path = os.path.abspath(ip_directory)
+def windows(ip_zip: Annotated[str, typer.Argument()]):
+    full_path = os.path.abspath(ip_zip)
 
-    if not os.path.isdir(full_path):
-        err_console.print("The chosen path is not a directory.")
+    if not full_path.endswith(".zip"):
+        err_console.print("The chosen path is not a zip file.")
         raise typer.Exit(code=1)
 
-    if convert:
-        if not utils.convert_files_to_utf8(full_path):
-            err_console.print("Something went wrong when converting files to UTF-8.")
-            raise typer.Exit(code=1)
+    parent_dir = os.path.dirname(full_path)
+    ip = os.path.splitext(os.path.basename(full_path))[0]
+    target_dir = os.path.join(parent_dir, ip)
 
-    ip = os.path.basename(full_path)
+    with zipfile.ZipFile(full_path, "r") as zip_ref:
+        zip_ref.extractall(target_dir)
+
+    if not utils.convert_files_to_utf8(target_dir):
+        err_console.print("Something went wrong when converting files to UTF-8.")
+        raise typer.Exit(code=1)
 
     # Files to be processed
-    systeminfo_txt = os.path.join(full_path, f"systeminfo_{ip}.txt")
-    discos_txt = os.path.join(full_path, f"discos_{ip}.txt")
-    puertos_txt = os.path.join(full_path, f"puertos_{ip}.txt")
-    programas_txt = os.path.join(full_path, f"programas_instalados__{ip}.txt")
+    systeminfo_txt = os.path.join(target_dir, f"systeminfo_{ip}.txt")
+    discos_txt = os.path.join(target_dir, f"discos_{ip}.txt")
+    puertos_txt = os.path.join(target_dir, f"puertos_{ip}.txt")
+    programas_txt = os.path.join(target_dir, f"programas_instalados__{ip}.txt")
+
+    output_file = os.path.join(target_dir, f"{ip}.txt")
 
     content = utils.get_summary(
         systeminfo_txt,
@@ -48,7 +51,7 @@ def windows(
 
     content += utils.get_programs(programas_txt)
 
-    with open(os.path.join(full_path, f"{ip}.txt"), "w") as file:
+    with open(output_file, "w") as file:
         file.write(content)
 
 
